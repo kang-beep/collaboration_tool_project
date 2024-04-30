a9c82ba8f0c821ba590883baed14c997a1484bbd
wwquen3568 <wwquen3568@naver.com>
2024-04-30 23:13:07 +0900

    design: doc_summary 결과 표시 페이지 추가

--- django_websocket_chatting/docsumm/urls.py
+++ django_websocket_chatting/docsumm/urls.py
@@ -6,5 +6,6 @@ from .views import *
 app_name = "docsumm"
 
 urlpatterns = [
-    path("", generate_doc_summ, name="generate-doc-summ")
+    path("", generate_doc_summ, name="generate-doc-summ"),
+    path("result/", result_doc_summ, name="result-doc-summ")
 ]
--- django_websocket_chatting/docsumm/views.py
+++ django_websocket_chatting/docsumm/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest
 
 
 
 def generate_doc_summ(request: HttpRequest):
+    """
+    문서 요약 하는 페이지
+    
+    """
+    
+    if request.method == "POST":
+        return redirect("docsumm:result-doc-summ")
+    
     return render(request, "docsumm/generate_doc_summ.html")
+
+
+
+def result_doc_summ(request: HttpRequest):
+    """
+    문서 결과 표기하는 페이지
+    
+    """
+    
+    context = {
+        
+    }
+    
+    return render(request, "docsumm/result_doc_summ.html", context)
+    
