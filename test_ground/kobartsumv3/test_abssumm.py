ba426e6fad3a2224c93b3b1bf9ee7eb3ee6b850c
wwquen3568 <wwquen3568@naver.com>
2024-05-01 11:55:39 +0900

    chore: 테스트파일 로직 및  이름 변경

--- test_ground/kobartsumv3/test_abssumm.py
+++ test_ground/kobartsumv3/test_abssumm.py
@@ -1,5 +1,7 @@
 from transformers import PreTrainedTokenizerFast, BartForConditionalGeneration
 
+
+
 # Load Model and Tokenizer
 tokenizer = PreTrainedTokenizerFast.from_pretrained("EbanLee/kobart-summary-v3")
 model = BartForConditionalGeneration.from_pretrained("EbanLee/kobart-summary-v3")
@@ -10,16 +12,16 @@ inputs = tokenizer(input_text, return_tensors="pt", padding="max_length", trunca
 
 # Generate Summary Text Ids
 summary_text_ids = model.generate(
-input_ids=inputs['input_ids'],
-attention_mask=inputs['attention_mask'],
-bos_token_id=model.config.bos_token_id,
-eos_token_id=model.config.eos_token_id,
-length_penalty=1.0,
-max_length=300,
-min_length=12,
-num_beams=6,
-repetition_penalty=1.5,
-no_repeat_ngram_size=15,
+    input_ids=inputs['input_ids'],
+    attention_mask=inputs['attention_mask'],
+    bos_token_id=model.config.bos_token_id,
+    eos_token_id=model.config.eos_token_id,
+    length_penalty=1.0,
+    max_length=300,
+    min_length=12,
+    num_beams=6,
+    repetition_penalty=1.5,
+    no_repeat_ngram_size=15,
 )
 
 # Decoding Text Ids
