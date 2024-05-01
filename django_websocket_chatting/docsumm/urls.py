03e4bdd37b2bf9dc53e27e4e8341208bd7cfc1a2
wwquen3568 <wwquen3568@naver.com>
2024-05-01 12:49:23 +0900

    chore: AJAX 적용에 따른 불필요한 결과페이지 삭제
    단일페이지에서 AJAX 요청하여 결과를 받기 때문에 불필요한 별도의 결과 표기 페이지를 삭제

--- django_websocket_chatting/docsumm/urls.py
+++ django_websocket_chatting/docsumm/urls.py
@@ -7,5 +7,4 @@ app_name = "docsumm"
 
 urlpatterns = [
     path("", generate_doc_summ, name="generate-doc-summ"),
-    path("result/", result_doc_summ, name="result-doc-summ")
 ]
--- django_websocket_chatting/docsumm/views.py
+++ django_websocket_chatting/docsumm/views.py
@@ -13,18 +13,3 @@ def generate_doc_summ(request: HttpRequest):
         return redirect("docsumm:result-doc-summ")
     
     return render(request, "docsumm/generate_doc_summ.html")
-
-
-
-def result_doc_summ(request: HttpRequest):
-    """
-    문서 결과 표기하는 페이지
-    
-    """
-    
-    context = {
-        
-    }
-    
-    return render(request, "docsumm/result_doc_summ.html", context)
-    
