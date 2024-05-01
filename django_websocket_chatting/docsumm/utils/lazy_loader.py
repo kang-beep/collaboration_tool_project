bf8184e607630acf43e7605525805890019a43d4
wwquen3568 <wwquen3568@naver.com>
2024-05-01 12:35:58 +0900

    feat: 토크나이저 및 모델을 로드하는 LazyLoader 구현
    싱글톤 패턴으로 구현
    
    토크나이저 또는 모델 객체가 실제로 필요할때 로드함으로써, Django 리로딩 시간을 줄일 수 있음.

---
+++ django_websocket_chatting/docsumm/utils/lazy_loader.py
@@ -0,0 +1,110 @@
+"""
+싱글톤 패턴으로 구현된 토크나이저 및 모델 로더.
+실제로 필요할 때, 토큰 및 모델 객체를 반환하게 해줌으로써,
+Django 리로딩 시간 및 views 렌더링 시간을 줄임.
+ 
+"""
+
+
+
+import torch
+from transformers import (
+    PreTrainedTokenizerFast, BartForConditionalGeneration,
+    BertTokenizer, BertModel
+)
+
+class LazyLoader():
+    """
+    싱글톤 패턴으로 구현된 토크나이저 및 모델 로더.
+    실제로 필요할 때, 토큰 및 모델 객체를 반환하게 해줌으로써,
+    Django 리로딩 시간 및 views 렌더링 시간을 줄임.
+    
+    """
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(LazyLoader, cls).__new__(cls)
+            
+            ## Abstractive Summary.
+            cls._instance.kobart_tokenizer = None
+            cls._instance.kobart_model = None
+            
+            ## Extractive Summary.
+            cls._instance.kobert_tokenizer = None
+            cls._instance.kobert_model = None
+
+        return cls._instance
+
+
+    def get_kobart_tokenizer(self):
+        """
+        싱글톤으로 이루어진 KoBART 토크나이저 객체 반환.
+    
+        Returns:
+            - 토크나이저 객체 반환.
+            
+        """
+        if self.kobart_tokenizer is None:
+            self.kobart_tokenizer = PreTrainedTokenizerFast.from_pretrained("EbanLee/kobart-summary-v3")
+            
+        return self.kobart_tokenizer
+
+
+    def get_kobart_model(self):
+        """
+        싱글톤으로 이루어진 KoBART 모델 객체 반환.
+    
+        Returns:
+            - 토크나이저 객체 반환.
+            
+        """
+        if self.kobart_model is None:
+            self.kobart_model = BartForConditionalGeneration.from_pretrained("EbanLee/kobart-summary-v3")
+            self.kobart_model.eval()  # 추론 모드로 설정
+            
+        return self.kobart_model
+
+
+    def get_kobert_tokenizer(self):
+        """
+        싱글톤으로 이루어진 KoBert 모델 객체 반환.
+    
+        Returns:
+            - 토크나이저 객체 반환.
+            
+        """        
+        if self.kobert_tokenizer is None:
+            self.kobert_tokenizer = BertTokenizer.from_pretrained("monologg/kobert")
+            
+        return self.kobert_tokenizer
+
+
+    def get_kobert__model(self):
+        """
+        싱글톤으로 이루어진 KoBert 모델 객체 반환.
+    
+        Returns:
+            - 토크나이저 객체 반환.
+            
+        """        
+        if self.kobert_model is None:
+            self.kobert_model = BertModel.from_pretrained("monologg/kobert")
+            self.kobert_model.eval()  # 추론 모드로 설정
+            
+        return self.kobert_model
+
+
+
+
+"""
+사용법.
+
+from lazy_load_model import ModelLoader
+
+model_loader = ModelLoader()
+model = model_loader.get_model()
+tokenizer = model_loader.get_tokenizer()
+
+
+"""
