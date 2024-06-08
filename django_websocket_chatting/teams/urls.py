a2323d72e4983e1f31e0164d414f605b5658c38f
chogunhee1 <chogh001@naver.com>
2024-06-08 22:04:57 +0900

    create my branch

--- django_websocket_chatting/teams/urls.py
+++ django_websocket_chatting/teams/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # group
     path('teams_list/', views.teams_list, name='teams_list'),
     path('teams_create/', views.teams_create, name='teams_create'),
+    path('edit_team/<int:team_id>/', views.edit_team, name='edit_team'),
+    path('delete_team/<int:team_id>/', views.delete_team, name='delete_team'),
 ]
--- django_websocket_chatting/teams/views.py
+++ django_websocket_chatting/teams/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
+import json
 from django.shortcuts import render, redirect
-
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_http_methods
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 
 from rest_framework.decorators import api_view
-from rest_framework import serializers,status
+from rest_framework import serializers, status
 from rest_framework.response import Response
 
 from .serializers import TeamSerializer
 from .models import Team
-# Create your views here.
 
 @login_required
 @api_view(['GET'])
@@ -38,3 +39,20 @@ def teams_create(request):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+    
+def edit_team(request, team_id):
+    if request.method == 'PUT':
+        data = json.loads(request.body)
+        team = get_object_or_404(Team, id=team_id)
+        team.name = data['name']
+        team.save()
+        return JsonResponse({'status': 'success'})
+    return JsonResponse({'status': 'fail'}, status=400)
+
+
+def delete_team(request, team_id):
+    if request.method == 'DELETE':
+        team = get_object_or_404(Team, id=team_id)
+        team.delete()
+        return JsonResponse({'status': 'success'})
+    return JsonResponse({'status': 'fail'}, status=400)
