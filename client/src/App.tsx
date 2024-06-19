692c471325b57286a421e21915421d7a67b30e33
kang-beep <kagsan0629@naver.com>
2024-06-20 01:33:26 +0900

    feat

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
-import StackNavigation from './navigation/StackNavigation';
-import { NavigationContainer } from '@react-navigation/native';
-import DrawerNavigator from './navigation/DrawerNavigator';
-import 'react-native-gesture-handler'
+import React, { useRef, useState, useEffect } from 'react';
+import { BackHandler, Alert } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-export default function App() {
+const MyWebView = () => {
+  const webViewRef = useRef(null);
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  useEffect(() => {
+    const backHandler = BackHandler.addEventListener(
+      'hardwareBackPress',
+      () => {
+        if (canGoBack && webViewRef.current) {
+          webViewRef.current.goBack();
+          return true;
+        } else {
+          Alert.alert(
+            '종료하시겠어요?',
+            '앱을 종료하시겠습니까?',
+            [
+              { text: '취소', onPress: () => {}, style: 'cancel' },
+              { text: '확인', onPress: () => BackHandler.exitApp() },
+            ],
+            { cancelable: false }
+          );
+          return true;
+        }
+      }
+    );
+
+    return () => backHandler.remove();
+  }, [canGoBack]);
+
   return (
-    <NavigationContainer>
-      <DrawerNavigator />
-    </NavigationContainer>
-    
+    <WebView
+      ref={webViewRef}
+      source={{ uri: 'http://203.252.230.246:8444/' }}
+      style={{ marginTop: 0 }}
+      onNavigationStateChange={(navState) => setCanGoBack(navState.canGoBack)}
+    />
   );
-}
+};
+
+export default MyWebView;
